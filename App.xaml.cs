edfd0d7d5be405a940feb004a359bfd27fa390e2
HardWar3 <mikewegner@gmx.net>
2023-12-27 14:15:17 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
+
+namespace noCapsLock_Tool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            NotifyIcon icon = new()
+            {
+                Icon = noCapsLock_Tool.Properties.Resources.Icon1,
+                Visible = true
+            };
+
+            icon.MouseClick += Icon_MouseClick;
+
+            base.OnStartup(e);
+
+        }
+
+        private void Icon_MouseClick(object? sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    System.Windows.MessageBox.Show("KEKSE LEFT");
+                    break;
+                case MouseButtons.Right:
+                    System.Windows.MessageBox.Show("KEKSE RIGHT");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace noCapsLock_Tool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (Console.ReadKey().Modifiers == ConsoleModifiers.Shift)
+            {
+                System.Windows.MessageBox.Show("KEKSEEEEEEEE CAPSSLOCK");
+            }
+        }
+    }
+}
